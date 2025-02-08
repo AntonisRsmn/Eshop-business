5fc1bbe289a69621d932294fe722d2ef8ac1cf98
Antonis Rusman <75878415+AntonisRsmn@users.noreply.github.com>
2025-02-09 00:27:20 +0200

    Update 09/02/2024 00:27

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const customersFilePath = path.join(__dirname, 'customers.json');
 
 // Serve static files from the root directory
